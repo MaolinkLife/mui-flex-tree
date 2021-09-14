a51d2cf23286adcc1a3545f9c2435da7c12badc9
MaolinkLife <maolink686@gmail.com>
2021-09-14 15:43:39 +0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { FlattenTreeNode } from './interfaces/tree.interface';
+import { FlattenTreeNode, TreeNode } from './interfaces/tree.interface';
 import { TREE_DATA } from './tree-data';
+
+enum TYPES {
+  'TYPE_1',
+  'TYPE_2'
+}
 
 @Component({
   selector: 'my-app',
@@ -16,7 +21,34 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.tree$.next(TREE_DATA);
+    const tree = this.covertedTree(TREE_DATA);
+    console.log('converted', tree);
+    this.tree$.next(tree);
+  }
+
+  covertedTree(node: TreeNode): FlattenTreeNode {
+    let convertedNodes: FlattenTreeNode[] = [];
+
+    if (node.children && node.children.length > 0) {
+      convertedNodes = node.children.map((childNode: TreeNode) =>
+        this.covertedTree(childNode)
+      );
+    }
+
+    const flattenNode: FlattenTreeNode = {
+      expandable: node.children && node.children.length > 0,
+      data: {
+        name: node.data.name,
+        id: node.data.id,
+        type: node.data.type
+      },
+      isExpand: node.data.type === 'TYPE_1' ? true : false,
+      isSelect: false,
+      type: TYPES[node.data.type],
+      ...(convertedNodes.length > 0 ? { children: convertedNodes } : {})
+    };
+
+    return flattenNode;
   }
 
   unSelectTreeNodes(node: FlattenTreeNode, currentNode: FlattenTreeNode): void {
